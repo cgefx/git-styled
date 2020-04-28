026bffd194ed2f9916e6246e684eb5ed2e078d44
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-04-27 17:25:07 -0700

    change form component to use name attribute

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -3,44 +3,40 @@ import { useHistory } from 'react-router-dom';
 import Octicon, { MarkGithub } from '@primer/octicons-react';
 
 const UserForm = () => {
-  const [username, setUsername] = useState('');
-  const history = useHistory();
+	const [username, setUsername] = useState('');
+	const history = useHistory();
 
-  const handleChange = e => setUsername(e.target.value);
-  const handleSubmit = () => history.push(`/user/${username}`);
+	const handleChange = e => setUsername(e.target.value);
+	const handleSubmit = e => {
+		e.preventDefault();
+		history.push(`/user/${username}`);
+	};
 
-  return (
-    <div className="user-form">
-      <div className="icon-wrapper">
-        <Octicon icon={MarkGithub} size="large" />
-      </div>
+	return (
+		<div className='user-form'>
+			<div className='icon-wrapper'>
+				<Octicon icon={MarkGithub} size='large' />
+			</div>
 
-      <form
-        className="user-form__form"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="username">
-          Github is kinda boring.
-          Let's style your
-          profile
-      </label>
+			<form className='user-form__form' onSubmit={handleSubmit}>
+				<label htmlFor='username'>
+					Get a styled version of your Github profile...
+				</label>
 
-        <input
-          type="text"
-          name="query"
-          placeholder="type a username..."
-          onChange={handleChange}
-        />
+				<input
+					type='text'
+					id='username'
+					name='username'
+					placeholder='type a username...'
+					onChange={handleChange}
+				/>
 
-        <button
-          className="user-form__button"
-          type="submit">
-          Git Styled!
-      </button>
-
-      </form>
-    </div>
-  );
+				<button className='user-form__button' type='submit'>
+					Git Styled!
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default UserForm;
